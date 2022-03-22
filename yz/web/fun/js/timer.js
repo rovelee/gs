4a0adcbdf31656fe9bc5e89f2a38557f6ff643f2
YoungZ <1308733129@qq.com>
2022-03-22 09:11:19 +0800

    '周期性定时器'

---
+++ yz/web/fun/js/timer.js
@@ -0,0 +1,12 @@
+// 声明变量存储定时器执行次数
+var i=1;
+// 设置周期定时器，每隔3s执行一次
+var timer=setInterval(function(){
+	console.log('Hello!');
+	// 每执行一次次数加一
+	i++;
+	// 当执行次数大于三时，执行清除定时器
+	if(i>3){
+	clearInterval(timer);
+}
+},3000);
